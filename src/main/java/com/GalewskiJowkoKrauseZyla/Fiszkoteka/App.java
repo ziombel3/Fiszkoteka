85a49d406d81ee4db38bcbb50f75e3baf128f842
Avocado20 <galewskirobert@gmail.com>
2016-04-10 16:28:05 +0200

    Init

---
+++ src/main/java/com/GalewskiJowkoKrauseZyla/Fiszkoteka/App.java
@@ -0,0 +1,13 @@
+package com.GalewskiJowkoKrauseZyla.Fiszkoteka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
